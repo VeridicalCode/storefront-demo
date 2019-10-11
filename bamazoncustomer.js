4945f6c95e7f708058f0feb53c22389a62415924
quething <m.getsoian@gmail.com>
2019-10-10 18:42:23 -0700

    more code work

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -50,82 +50,36 @@ function promptUserAction() {
         })
         .then(function (answer) {
             // query SQL for that specific item
-            connection.query("SELECT * FROM products WHERE ?", [{id:answer.itemID}], function (err, results) {
-                if (err) throw err;
+            connection.query("SELECT * FROM products WHERE ?",
+                [{ item_id: answer.itemID }]
+                , function (err, results) {
+                    if (err) throw err;
+                    let currentStock = results[i].stock_quantity;
 
-            
-                if (answer.quantity > `current number`) {
-                    console.log(`We're sorry, we don't currently have that many ${itemname} in stock.`);
-                }
-                else {
-                    let totalprice = answer.quantity * itemprice;
-                    console.log(`Thank you for your purchase! ${totalprice} will be deducted from your account.`);
-                    printStorefront();
-                }
-            }
+                    results[i].whatever
+                    if (answer.quantity > currentStock) {
+                        console.log(`We're sorry, we don't currently have that many ${results[i].product_name} in stock.`);
+                    }
+                    else {
+                        let totalprice = answer.quantity * results[i].price;
+                        console.log(`Thank you for your purchase! ${totalprice} will be deducted from your account.`);
+                        printStorefront();
+                        finalizeItemPurchase(answer.itemID, answer.quantity, currentStock);
+                    }
+                })
         });
 }
 
+// function to deduct product from SQL server
+function finalizeItemPurchase(itemID, quantity, stock) {
+    let newQuantity = stock - quantity;
+    connection.query("SET ? FROM products WHERE ?",
+    [{stock_quantity:newQuantity},{item_id:itemID}],
+    function(err){
+        if (err) throw (err)
+    })
+}
 
-function bidAuction() {
-    // query the database for all items being auctioned
-    connection.query("SELECT * FROM auctions", function (err, results) {
-        if (err) throw err;
-        // once you have the items, prompt the user for which they'd like to bid on
-        inquirer
-            .prompt([
-                {
-                    name: "choice",
-                    type: "rawlist",
-                    choices: function () {
-                        var choiceArray = [];
-                        for (var i = 0; i < results.length; i++) {
-                            choiceArray.push(results[i].item_name);
-                        }
-                        return choiceArray;
-                    },
-                    message: "What auction would you like to place a bid in?"
-                },
-                {
-                    name: "bid",
-                    type: "input",
-                    message: "How much would you like to bid?"
-                }
-            ])
-            .then(function (answer) {
-                // get the information of the chosen item
-                var chosenItem;
-                for (var i = 0; i < results.length; i++) {
-                    if (results[i].item_name === answer.choice) {
-                        chosenItem = results[i];
-                    }
-                }
 
-                // determine if bid was high enough
-                if (chosenItem.highest_bid < parseInt(answer.bid)) {
-                    // bid was high enough, so update db, let the user know, and start over
-                    connection.query(
-                        "UPDATE auctions SET ? WHERE ?",
-                        [
-                            {
-                                highest_bid: answer.bid
-                            },
-                            {
-                                id: chosenItem.id
-                            }
-                        ],
-                        function (error) {
-                            if (error) throw err;
-                            console.log("Bid placed successfully!");
-                            start();
-                        }
-                    );
-                }
-                else {
-                    // bid wasn't high enough, so apologize and start over
-                    console.log("Your bid was too low. Try again...");
-                    start();
-                }
-            });
-    });
+    })
 }
