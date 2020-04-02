2d51f353e1c62443505a70fa4096cf27aae8e08f
Mars <m.getsoian@gmail.com>
2020-04-02 15:01:07 -0700

    added extremely rudimentary seed file

---
+++ assets/seeds.sql
@@ -0,0 +1,9 @@
+USE bamazon;
+INSERT INTO products (item_id, product_name, price, stock_quantity)
+VALUES ("Falafel Iron", 88.52, 10);
+
+INSERT INTO products (item_id, product_name, price, stock_quantity)
+VALUES ("Bonsai Tree", 12.50, 77);
+
+INSERT INTO products (item_id, product_name, price, stock_quantity)
+VALUES ("Commemerative Mug", 7.99, 22);
